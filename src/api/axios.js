5cd78826a7e408c59ffbe96f53a7e29cfe36be72
Hyeonju Kang <46iz.us2@gmail.com>
2023-01-04 23:02:46 +0900

    build test

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
-import { API_KEY } from './apikey.js'
+import dotenv from 'dotenv'
+// import { API_KEY } from './apikey.js'
 
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: API_KEY,
+    api_key: process.env.API_KEY,
     language: "ko-KR",
   }
 })
