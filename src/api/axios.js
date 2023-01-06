a2a2c8b64c9dda9aee9a0937db7b188566a1bbbf
Hyeonju Kang <46iz.us2@gmail.com>
2023-01-06 15:01:33 +0900

    Fix apikey variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
-import dotenv from 'dotenv'
-// import { API_KEY } from './apikey.js'
-
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: process.env.API_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
     language: "ko-KR",
   }
 })
